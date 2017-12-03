b0b045c9de1f553298130adb102c27a39a327182
albin234 <gopalgenjimagar@gmail.com>
2017-12-03 17:28:44 +0545

    proof of equation

---
+++ equation.py
@@ -0,0 +1,8 @@
+def LHS(a,b):
+    return(a+b)**2
+def RHS(a,b):
+    return a**2+2*a*b+b**2
+l=LHS(2,3)
+r=RHS(2,3)
+print("LHS is eqaul to {} and RHS is equal to {}".format(l,r))
+print("LHS is Equal To RHS")
